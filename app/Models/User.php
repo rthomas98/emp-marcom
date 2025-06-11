0516a0b3cf58e82b28e53c813710ff6c447bc28e
Rob Thomas <robt84@gmail.com>
2025-06-11 13:26:43 -0500

    fixing filament issues

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Notifications\CustomResetPasswordNotification;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
@@ -57,5 +59,21 @@ class User extends Authenticatable
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new CustomResetPasswordNotification($token));
+    }
+    
+    /**
+     * Determine if the user can access the Filament panel.
+     *
+     * @param Panel $panel
+     * @return bool
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Allow access to all authenticated users for now
+        // You can customize this later with more specific conditions
+        return true;
+        
+        // Example of more restrictive access:
+        // return $this->hasRole('admin') || $this->email === 'rob.thomas@empuls3.com';
     }
 }
